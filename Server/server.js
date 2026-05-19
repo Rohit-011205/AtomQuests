93fad8ab09017eddfcc6dfc5118e456dc2ffad79
Rohit Kadam <upskillrohit@gmail.com>
2026-05-19 09:28:47 +0530

    URL fix

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ import checkinRoutes from './routes/checkinRoutes.js'
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
    credentials: true     
 }));
 app.use(express.json());
